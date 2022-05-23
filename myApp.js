9ea2a44b6b0fe45f65e89a437c1e8f2d6ab38e1d
LAP13821-local <tramptb@vng.com.vn>
2022-05-23 13:40:09 +0700

    Serve an HTML File

--- myApp.js
+++ myApp.js
@@ -1,43 +1,12 @@
 let express = require('express');
 let app = express();
 //console.log("Hello World");
-app.get('/', function(req, res) {
+/*app.get("/", function(req, res) {
  res.send('Hello Express');
+});*/
+const absolutePath = __dirname + '/views/index.html';
+app.get("/", function (req, res) {
+    res.sendFile(absolutePath);
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
