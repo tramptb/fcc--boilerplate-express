68b3fc334dd489b2537b69c3787b762ff5e7e33f
LAP13821-local <tramptb@vng.com.vn>
2022-05-23 11:49:08 +0700

    Start a Working Express Server

--- myApp.js
+++ myApp.js
@@ -1,9 +1,9 @@
 let express = require('express');
 let app = express();
 //console.log("Hello World");
-app.get("/", function(req, res) {
+app.get('/', function(req, res) {
  res.send('Hello Express');
-})
+});
 
 
 
