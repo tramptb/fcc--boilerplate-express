e727f296244f0acb9cd61f3b1407ed52cc3ffcc7
LAP13821-local <tramptb@vng.com.vn>
2022-05-27 13:53:05 +0700

    Serve Static Assets

--- myApp.js
+++ myApp.js
@@ -8,6 +8,6 @@ let app = express();
 app.get("/", function (req, res) {
     res.sendFile(absolutePath);
 });*/
-app.use(express.static(__dirname + '/public'));
+app.use('/public', express.static(__dirname + "/public"));
 
 module.exports = app;
