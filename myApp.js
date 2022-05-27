343c6579fa426f032bbce21da1e95cdb8db6b47b
LAP13821-local <tramptb@vng.com.vn>
2022-05-27 14:27:49 +0700

    Use the .env File

--- myApp.js
+++ myApp.js
@@ -1,12 +1,17 @@
+require('dotenv').config();
 let express = require('express');
 let app = express();
 //console.log("Hello World");
 /*app.get("/", function(req, res) {
  res.send('Hello Express');
 });*/
-const absolutePath = __dirname + '/views/index.html';
+//const absolutePath = __dirname + '/views/index.html';
+let message = "Hello json";
+if(process.env.MESSAGE_STYLE === 'uppercase'){
+    message = message.toUpperCase();
+}
 app.get("/json", function (req, res) {
-    res.json({"message": "Hello json"});
+    res.json({"message": message});
 });
 /*app.use('/public', express.static(__dirname + "/public"));*/
 
