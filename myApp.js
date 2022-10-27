ce1624af3f8f9e904f189a5d9a53659fb21d0981
LAP13821-local <tramptb@vng.com.vn>
2022-10-27 21:49:33 +0700

    Use the .env File

--- myApp.js
+++ myApp.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 let express = require('express');
 let app = express();
+let bodyParser = require('body-parser');
 //console.log("Hello World");
 /*app.get("/", function(req, res) {
  res.send('Hello Express');
@@ -17,9 +18,56 @@ let app = express();
 });*/
 /*app.use('/public', express.static(__dirname + "/public"));*/
 
-app.use( (req, res, next)=>{
-    console.log(req.method + " "+ req.path + "-" + req.ip);
-    next();
-})
+//Start Implement a Root-Level Request Logger Middleware
+/*let myLogger = (req, res, next) => {
+    let ClientIp = req.ip
+    let path = req.path
+    let method = req.method
 
+    console.log(method + " " + path + " - " + ClientIp);
+    next()
+}
+app.use(myLogger);*/
+//End Implement a Root-Level Request Logger Middleware
+
+//Chain Middleware to Create a Time Server
+/*app.get('/now', function(req, res, next) {
+    req.time = new Date().toString();  // Hypothetical synchronous operation
+    next();
+}, function(req, res) {
+    res.send(res.send({time: req.time}));
+});*/
+//End Chain Middleware to Create a Time Server
+
+//Get Route Parameter Input from the Client
+/*app.get("/:word/echo", (req, res) => {
+    const { word } = req.params;
+    res.json({
+        echo: word
+    });
+});*/
+//End Get Route Parameter Input from the Client
+
+//Start Get Query Parameter Input from the Client
+/*app.get("/name", (req, res) => {
+    let firstName = req.query.first;
+    let lastName = req.query.last;
+    // OR you can destructure and rename the keys
+    //var { first: firstName, last: lastName } = req.query;
+    // Use template literals to form a formatted string
+    res.json({
+        name: `${firstName} ${lastName}`
+    });
+});*/
+//End Get Query Parameter Input from the Client
+
+//Start Use body-parser to Parse POST Requests
+app.post("/name", (req, res) => {
+    let string = req.body.first + " " + req.body.last;
+    res.json({ name: string });
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+//End Use body-parser to Parse POST Requests
 module.exports = app;
